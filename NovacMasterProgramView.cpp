3dc2061a74b634b2382410ea4a5e83cf9e7a564c
Diana Norgaard <dnorgaard@usgs.gov>
2019-03-19 15:25:08 -0700

    Resize main frame and status log/frame better

--- NovacMasterProgramView.cpp
+++ NovacMasterProgramView.cpp
@@ -956,7 +956,7 @@ int CNovacMasterProgramView::InitializeControls(){
 	// Move everything into place...
 	m_masterFrame.GetWindowRect(rect);
 	GetWindowRect(rect2);
-	m_sheet.MoveWindow(rect2.left, rect.top - rect2.top, rect2.Width(), rect.Height());
+	m_sheet.MoveWindow(rect.left, 5 + rect.top - rect2.top, rect2.Width(), rect.Height());
 	m_masterFrame.GetWindowRect(rect);
 
 	// Get the tab control and set the title of each tab to the serial number of the spectrometer
@@ -1448,3 +1448,5 @@ LRESULT CNovacMasterProgramView::OnRewriteConfigurationXml(WPARAM wParam, LPARAM
 
 	return 0;
 }
+
+
